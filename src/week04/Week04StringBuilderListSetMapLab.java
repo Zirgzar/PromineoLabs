a8632d6955696062df685db8638c2709526c9d4a
Seth Tor <sethtor@Seths-MacBook-Pro.local>
2023-12-12 15:56:20 -0800

    Finish week 4 labs, init week 5

--- src/week04/Week04StringBuilderListSetMapLab.java
+++ src/week04/Week04StringBuilderListSetMapLab.java
@@ -36,7 +36,7 @@ public class Week04StringBuilderListSetMapLab {
 		
 		List<String> strList = new ArrayList<>();
 		
-		strList.add("ABCDEF");
+		strList.add("A");
 		strList.add("AB");
 		strList.add("ABC");
 		strList.add("ABCD");
@@ -53,16 +53,24 @@ public class Week04StringBuilderListSetMapLab {
 		
 		// 4. Write and test a method that takes a list of strings 
 		//			and returns the list with the first and last element switched
+		
+		swapFirstAndLastString(strList);
+		System.out.println(strList);
+		swapFirstAndLastString(strList);
 
 		
 		// 5. Write and test a method that takes a list of strings 
 		//			and returns a string with all the list elements concatenated to each other,
 		// 			separated by a comma
+		
+		System.out.println(concatenateListString(strList));
 
 		
 		// 6. Write and test a method that takes a list of strings and a string 
 		//			and returns a new list with all strings from the original list
 		// 			containing the second string parameter (i.e. like a search method)
+		
+		System.out.println(findInListString(strList, "BC"));
 
 		
 		// 7. Write and test a method that takes a list of integers 
@@ -73,90 +81,198 @@ public class Week04StringBuilderListSetMapLab {
 		//		b. The second List contains values from the input list that are divisible by 3
 		//		c. The third containing values divisible by 5, and 
 		//		d. The fourth all numbers from the input List not divisible by 2, 3, or 5
+		
+		List<Integer> intList = Arrays.asList(2, 3, 5, 10, 13, 15, 18, 30, 37, 41);
+		System.out.println(seperateIntegerList(intList));
 
 		
 		// 8. Write and test a method that takes a list of strings 
 		//			and returns a list of integers that contains the length of each string
-
+		
+		System.out.println(findLengthOfListStrings(strList));
 
 		
 		// 9. Create a set of strings and add 5 values
 
+		Set<String> strSet = new HashSet<>();
+		strSet.add("Z");
+		strSet.add("YZ");
+		strSet.add("XYZ");
+		strSet.add("WXYZ");
+		strSet.add("zVWXYZ");
 		
 		
 		// 10. Write and test a method that takes a set of strings and a character 
 		//			and returns a set of strings consisting of all the strings in the
 		// 			input set that start with the character parameter.
 
+		System.out.println(findSetStringByFirstChar(strSet, 'Z'));
 
 		
 		// 11. Write and test a method that takes a set of strings 
 		//			and returns a list of the same strings
 	
+		List<String> newStrList = convertSetToList(strSet);
+		System.out.println(convertSetToList(strSet) instanceof List<String>);
+		System.out.println(newStrList instanceof List<String>);
 		
 
 		// 12. Write and test a method that takes a set of integers 
 		//			and returns a new set of integers containing only even numbers 
 		//			from the original set
 
+		Set<Integer> intSet = new HashSet<>(Arrays.asList(2, 4, 9, 10, 53, 69, 420));
+		System.out.println(makeEvenOnlySet(intSet));
 
 		
 		// 13. Create a map of string and string and add 3 items to it where the key of each
 		// 			is a word and the value is the definition of the word
 
+		Map<String, String> dictMap = new HashMap<>();
+		dictMap.put("love", "an intense feeling of deep affection.");
+		dictMap.put("hate", "intense or passionate dislike.");
+		dictMap.put("indifference", "lack of interest, concern, or sympathy.");
 	
 		
 		// 14. Write and test a method that takes a Map<String, String> and a string 
 		// 			and returns the value for a key in the map that matches the
 		// 			string parameter (i.e. like a language dictionary lookup)
 
+		System.out.println(findValue(dictMap, "love"));
+		
 		
 		// 15. Write and test a method that takes a List<String> 
 		//			and returns a Map<Character, Integer> containing a count of 
 		//			all the strings that start with a given character
 		
+		System.out.println(countStringsStartingWithChar(strList));
+		System.out.println(countStringsStartingWithChar(convertSetToList(strSet)));
 
+		
 	}
 	
 	
 	// Method 15:
-	
+	public static Map<Character, Integer> countStringsStartingWithChar(List<String> listString) {
+		Map<Character, Integer> charMap = new HashMap<>();
+		for (String s : listString) {
+			charMap.merge(s.toLowerCase().charAt(0), 1, Integer::sum);
+		}
+		return charMap;
+	}
 	
 	
 	// Method 14:
-	
+	public static String findValue(Map<String, String> mapString, String search) {
+		return mapString.get(search.toLowerCase());
+	}
 
 	
 	// Method 12:
-	
+	public static Set<Integer> makeEvenOnlySet(Set<Integer> setInt) {
+		Set<Integer> evens = new HashSet<>();
+		for (int i : setInt) {
+			if (i % 2 == 0) {
+				evens.add(i);
+			}
+		}
+		return evens;
+	}
 
 	
 	// Method 11:
-	
+	public static List<String> convertSetToList(Set<String> setString) {
+		List<String> listString = new ArrayList<>();
+		for (String s : setString) {
+			listString.add(s);
+		}
+		return listString;
+	}
 
 
 	// Method 10:
-	
+	public static Set<String> findSetStringByFirstChar(Set<String> setString, char search) {
+		Set<String> newSet = new HashSet<>();
+		for (String s : setString) {
+			if (s.toLowerCase().charAt(0) == Character.toLowerCase(search)) {
+				newSet.add(s);
+			}
+		}
+		return newSet;
+	}
 
 	
 	// Method 8:
-	
+	public static List<Integer> findLengthOfListStrings(List<String> listString) {
+		List<Integer> lengthList = new ArrayList<>();
+		for (String s : listString) {
+			lengthList.add(s.length());
+		}
+		return lengthList;
+	}
 
 	
 	// Method 7:
-	
+	public static List<List<Integer>> seperateIntegerList(List<Integer> listInt) {
+		List<Integer> divisibleByTwo = new ArrayList<>();
+		List<Integer> divisibleByThree = new ArrayList<>();
+		List<Integer> divisibleByFive = new ArrayList<>();
+		List<Integer> notDivisible = new ArrayList<>();
+		for (int i : listInt) {
+			if (i % 2 == 0) {
+				divisibleByTwo.add(i);
+			}
+			if (i % 3 == 0) {
+				divisibleByThree.add(i);
+			}
+			if (i % 5 == 0) {
+				divisibleByFive.add(i);
+			}
+			if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+				notDivisible.add(i);
+			}
+		}
+		List<List<Integer>> finalList = new ArrayList<>();
+		finalList.add(divisibleByTwo);
+		finalList.add(divisibleByThree);
+		finalList.add(divisibleByFive);
+		finalList.add(notDivisible);
+		return finalList;
+	}
 
 	
 	// Method 6:
-	
+	public static List<String> findInListString(List<String> listString, String search) {
+		List<String> newList = new ArrayList<>();
+		for (String s : listString) {
+			if (s.toLowerCase().contains(search.toLowerCase())) {
+				newList.add(s);
+			}
+		}
+		return newList;
+	}
 
 	
 	// Method 5:
-	
+	public static String concatenateListString(List<String> listString) {
+		String finalString = "";
+		for (String s : listString) {
+			if (s == listString.get(listString.size() - 1)) {
+				finalString += s;
+			} else {
+				finalString += s + ",";
+			}
+		}
+		return finalString;
+	}
 	
 	
 	// Method 4:
-	
+	public static void swapFirstAndLastString(List<String> listString) {
+		String temp = listString.get(0);
+		listString.set(0, listString.get(listString.size() - 1));
+		listString.set(listString.size() - 1, temp);
+	}
 	
 	
 	// Method 3:
